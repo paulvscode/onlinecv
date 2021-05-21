877848cb6804584c35691cebc005ffe1640ec3c8
paulvscode <67148083+paulvscode@users.noreply.github.com>
2021-05-21 12:12:13 +0200

    layout redesigned

---
+++ script.js
@@ -0,0 +1,24 @@
+// email Js
+
+(function () {
+  emailjs.init("user_0CrOptuBnm7Nb076RUCzg");
+})();
+
+window.onload = function () {
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      // generate a five digit number for the contact_number variable
+      this.contact_number.value = (Math.random() * 100000) | 0;
+      // these IDs from the previous steps
+      emailjs.sendForm("service_djxr31y", "template_rwzn49c", this).then(
+        function () {
+          console.log("SUCCESS!");
+        },
+        function (error) {
+          console.log("FAILED...", error);
+        }
+      );
+    });
+};
